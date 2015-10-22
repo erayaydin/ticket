f8304320bf8cc8a41d7495adc0c2b41657c05d3e
Eray Aydın <turkwebdeveloper@gmail.com>
2015-10-22 23:04:18 +0300

    Added File model and migration file

---
+++ database/migrations/2015_10_22_195922_create_files_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string("name");
+            $table->string("file");
+            $table->string("ext");
+            $table->bigInteger("size");
+            $table->string("type");
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('files');
+    }
+}
